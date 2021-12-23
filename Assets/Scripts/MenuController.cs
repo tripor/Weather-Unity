937e94c968a6e14b21ff1797d5f3d14d699039b4
tripor <figorifico3000@gmail.com>
2021-12-23 12:52:57 +0000

    Scene change buttons

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,25 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField]
+    private List<Button> ChangeSceneButtons;
 
+    public void ChangeScene(int sceneIdentifier)
+    {
+        SetInteractableChangeSceneButtons(false);
+        StartCoroutine(LoadSceneAsync(sceneIdentifier));
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator LoadSceneAsync(int sceneIdentifier)
     {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneIdentifier, LoadSceneMode.Single);
 
+        while (!asyncOperation.isDone)
+        {
+            yield return null;
+        }
+        SetInteractableChangeSceneButtons(true);
     }
 
+    private void SetInteractableChangeSceneButtons(bool status)
+    {
+        foreach (var button in ChangeSceneButtons)
+        {
+            button.interactable = status;
+        }
+    }
 
     public void ExitApplication()
     {
         Application.Quit(0);
-        return;
     }
 }
