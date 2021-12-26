7a77ba1a0177ee9e060454bac1fe5656551d37df
tripor <figorifico3000@gmail.com>
2021-12-26 13:43:45 +0000

    Clouds

---
+++ Assets/Scripts/LocationScripts/MovingCloud.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCloud : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float rotationSpeed = 1.0f;
+
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+    private float totalDistance;
+    private float startingTime;
+    private Vector3 rotateAmount;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = new Vector3(Random.Range(0.8f, 1.5f), Random.Range(0.8f, 1.5f), Random.Range(0.8f, 1.5f));
+        rotateAmount = new Vector3(Random.Range(0f, 1f) * rotationSpeed, Random.Range(0f, 1f) * rotationSpeed, Random.Range(0f, 1f) * rotationSpeed);
+        SetNewPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceSoFar = (Time.time - startingTime) * speed;
+        float percentageDistanceSoFar = distanceSoFar / totalDistance;
+        transform.rotation *= Quaternion.Euler(rotateAmount);
+        if (percentageDistanceSoFar >= 1)
+            SetNewPosition();
+        else
+            transform.position = Vector3.Lerp(startPosition, endPosition, percentageDistanceSoFar);
+    }
+
+    private void SetNewPosition()
+    {
+        startPosition = transform.position;
+        endPosition = new Vector3(Random.Range(-2f, 2f), transform.position.y, Random.Range(-2f, 2f));
+        totalDistance = Vector3.Distance(startPosition, endPosition);
+        startingTime = Time.time;
+    }
+}
--- Assets/Scripts/LocationScripts/WeatherController.cs
+++ Assets/Scripts/LocationScripts/WeatherController.cs
@@ -7,10 +7,15 @@ public class WeatherController : MonoBehaviour
     public GameObject cloudParent;
     public GameObject cloudPrefab;
 
+    [SerializeField]
+    private int totalNumberOfClouds = 20;
+
+    private List<GameObject> clouds;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        GenerateClouds();
     }
 
     // Update is called once per frame
@@ -23,6 +28,25 @@ public class WeatherController : MonoBehaviour
 
     private void GenerateClouds()
     {
-
+        List<GameObject> cloudToRemove = new List<GameObject>();
+        clouds = new List<GameObject>();
+        foreach (Transform item in cloudParent.transform)
+        {
+            if (item.tag == "Cloud")
+            {
+                cloudToRemove.Add(item.gameObject);
+            }
+        }
+        foreach (var cloud in cloudToRemove)
+        {
+            Destroy(cloud);
+        }
+        for (int i = 0; i < totalNumberOfClouds; i++)
+        {
+            var newCloud = Instantiate(cloudPrefab);
+            newCloud.transform.parent = cloudParent.transform;
+            newCloud.transform.position = new Vector3(Random.Range(-2f, 2f), cloudParent.transform.position.y, Random.Range(-2f, 2f));
+            clouds.Add(newCloud);
+        }
     }
 }
